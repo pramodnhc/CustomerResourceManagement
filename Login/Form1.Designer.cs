8560715aa0575ab9df24186298ea96519820f555
pramodnhc <pummy.2022@gmail.com>
2018-09-23 20:53:47 +0530

    Created Login

--- Login/Form1.Designer.cs
+++ Login/Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblLogin = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.checkBoxshowpassword = new System.Windows.Forms.CheckBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblLogin
+            // 
+            this.lblLogin.AutoSize = true;
+            this.lblLogin.Font = new System.Drawing.Font("Wide Latin", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLogin.Location = new System.Drawing.Point(123, 38);
+            this.lblLogin.Name = "lblLogin";
+            this.lblLogin.Size = new System.Drawing.Size(195, 34);
+            this.lblLogin.TabIndex = 0;
+            this.lblLogin.Text = "LOGIN";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.panel1.Controls.Add(this.lblLogin);
+            this.panel1.Location = new System.Drawing.Point(-19, 6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(447, 106);
+            this.panel1.TabIndex = 1;
+            // 
+            // txtusername
+            // 
+            this.txtusername.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtusername.Location = new System.Drawing.Point(67, 181);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(290, 30);
+            this.txtusername.TabIndex = 2;
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtpassword.Location = new System.Drawing.Point(67, 255);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.Size = new System.Drawing.Size(290, 30);
+            this.txtpassword.TabIndex = 3;
+            this.txtpassword.UseSystemPasswordChar = true;
+            // 
+            // checkBoxshowpassword
+            // 
+            this.checkBoxshowpassword.AutoSize = true;
+            this.checkBoxshowpassword.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.checkBoxshowpassword.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxshowpassword.Location = new System.Drawing.Point(67, 337);
+            this.checkBoxshowpassword.Name = "checkBoxshowpassword";
+            this.checkBoxshowpassword.Size = new System.Drawing.Size(137, 23);
+            this.checkBoxshowpassword.TabIndex = 4;
+            this.checkBoxshowpassword.Text = "Show Password";
+            this.checkBoxshowpassword.UseVisualStyleBackColor = true;
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlogin.Location = new System.Drawing.Point(235, 320);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(122, 40);
+            this.btnlogin.TabIndex = 5;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(426, 450);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.checkBoxshowpassword);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = " ";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblLogin;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtpassword;
+        private System.Windows.Forms.CheckBox checkBoxshowpassword;
+        private System.Windows.Forms.Button btnlogin;
     }
 }
 
